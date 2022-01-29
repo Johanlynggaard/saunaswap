d4038ac5ebb9ac6697fcc5fdc88f290c4ecec937
Chris Rundell <cdrundell@icloud.com>
2022-01-29 16:19:17 +0000

    corrected seeds for sauna

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,41 +7,52 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Sauna.destroy_all
+
+
 puts "Creating Users"
-User.create!(name: "Johan", email: "mipix@getairmail.com", password: "Frog123")
-User.create!(name: "Carlos", email: "hasofic@getnada.com", password: "Frog123")
-User.create!(name: "Chris", email: "rujyq@zetmail.com", password: "Frog123")
-User.create!(name: "Nik", email: "miqifigyt@vomoto.com", password: "Frog123")
+user_1 = User.create!(name: "Johan", email: "mipix@getairmail.com", password: "Frog123")
+user_2 = User.create!(name: "Carlos", email: "hasofic@getnada.com", password: "Frog123")
+user_3 = User.create!(name: "Chris", email: "rujyq@zetmail.com", password: "Frog123")
+user_4 = User.create!(name: "Nik", email: "miqifigyt@vomoto.com", password: "Frog123")
 puts "Users created!"
 
-# puts "Creating Saunas"
+puts "Creating Saunas"
 
-# Sauna.create!(title: "Amazing Swedish Sauna",
-#    address: "Stockholm, SE",
-#    price: "1000",
-#    capacity: 4,
-#    image_url: "https://unsplash.com/photos/a-0Qsar9rBg",
-#    description: "This is so hot!",
-#    sauna_type: "Swedish"
-#   )
+swedish = Sauna.new(title: "Amazing Swedish Sauna",
+   address: "Stockholm, SE",
+   price: "1000",
+   capacity: 4,
+   image_url: "https://unsplash.com/photos/a-0Qsar9rBg",
+   description: "This is so hot!",
+   sauna_type: "Swedish",
+   user_id: user_1.id
+  )
 
-#   Sauna.create!(title: "Mediocre Finish Sauna",
-#    address: "Oulu, FI",
-#    price: "800",
-#    capacity: 2,
-#    image_url: "https://unsplash.com/photos/a-0Qsar9rBg",
-#    description: "This is warm :(",
-#    sauna_type: "Finnish"
-#   )
+  swedish.save!
 
+  finnish =  Sauna.new(title: "Mediocre Finnish Sauna",
+   address: "Oulu, FI",
+   price: "800",
+   capacity: 2,
+   image_url: "https://unsplash.com/photos/a-0Qsar9rBg",
+   description: "This is warm :(",
+   sauna_type: "Finnish",
+   user_id: user_2.id
+  )
 
-#   Sauna.create!(title: "Turkish Sauna",
-#    address: "Istanbul, Turkey",
-#    price: "200",
-#    capacity: 6,
-#    image_url: "https://unsplash.com/photos/a-0Qsar9rBg",
-#    description: "Oriental sauna",
-#    sauna_type: "Turkish"
-#   )
+  finnish.save!
 
-# puts "Created Saunas"
+  turkish = Sauna.new(title: "Turkish Sauna",
+   address: "Istanbul, Turkey",
+   price: "200",
+   capacity: 6,
+   image_url: "https://unsplash.com/photos/a-0Qsar9rBg",
+   description: "Oriental sauna",
+   sauna_type: "Turkish",
+   user_id: user_3.id
+  )
+
+  turkish.save!
+
+puts "Created Saunas"
