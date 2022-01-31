014baebc68cd424bab70a6a1acd2b2dbc34ce0cf
Chris Rundell <cdrundell@icloud.com>
2022-01-31 16:29:58 +0000

    changed route for bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :sauna
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :status, presence: true
 end
