b96396fa2526b5d3f51dd1882c969a4f8976a170
Niklas Coors <coors.niklas@gmail.com>
2022-02-03 21:45:52 +0100

    Added initial draft and policy for user profile page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,12 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  def show?
+    return true
+    # @user.id == record.id
+    # @user == current_user
+  end
+end
