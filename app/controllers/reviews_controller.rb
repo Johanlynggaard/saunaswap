92853f26ba35ea3d1185b77019a3fa15be6e3eba
carlos diaz <diazmelian.carlos@gmail.com>
2022-02-05 10:33:58 +0000

    created new review action & view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  before_action :find_sauna
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def find_sauna
+    @sauna = Sauna.find(params[:sauna_id])
+  end
 end
