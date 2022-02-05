aeb5cb1f112d35810aca805062e5c2a78f2ef694
carlos diaz <diazmelian.carlos@gmail.com>
2022-02-05 10:39:25 +0000

    created create review action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,12 +6,25 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @raview.user = current_user
+    @review.sauna = @sauna
+    authorize @review
 
+    if @review.save
+      redirect_to user_path
+    else
+      render :new
+    end
   end
 
   private
 
   def find_sauna
     @sauna = Sauna.find(params[:sauna_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
