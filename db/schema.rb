c32438d4a19efc6f0f6853bedf7385ea301416a7
carlos diaz <diazmelian.carlos@gmail.com>
2022-02-05 10:16:07 +0000

    created AddRatingToSauna migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_174257) do
+ActiveRecord::Schema.define(version: 2022_02_05_101500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2022_02_01_174257) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "sauna_id", null: false
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sauna_id"], name: "index_reviews_on_sauna_id"
+  end
+
   create_table "saunas", force: :cascade do |t|
     t.string "title"
     t.text "address"
@@ -66,6 +75,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_174257) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
     t.index ["user_id"], name: "index_saunas_on_user_id"
   end
 
@@ -89,5 +99,6 @@ ActiveRecord::Schema.define(version: 2022_02_01_174257) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "saunas"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "saunas"
   add_foreign_key "saunas", "users"
 end
