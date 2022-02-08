9610ea7ba972c16e044fec8d12230ad5c041bd95
Johan Lynggaard <johan.lynggaard@gmail.com>
2022-02-08 19:22:37 +0100

    Created /profile path for user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :update]
-
+  # resources :users, only: [:edit, :update]
+  get '/profile', to: 'users#show'
+  get '/profile/edit', to: 'users#edit'
+  post '/profile/edit', to: 'users#update'
   resources :saunas do
     resources :bookings
     resources :reviews, only: [:new, :create]
