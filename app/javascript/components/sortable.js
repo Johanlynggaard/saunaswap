4ecf31212c122c76d33be195a540ef08f805d9bd
Chris Rundell <cdrundell@icloud.com>
2022-02-12 15:03:08 +0000

    fixed js

--- app/javascript/components/sortable.js
+++ app/javascript/components/sortable.js
@@ -3,7 +3,7 @@ const asc = () => {
   let btn = document.getElementById("ascending");
   btn.addEventListener("click", (event) => {
     const elements = document.querySelectorAll(".card-trip");
-    const cards = document.querySelector("#content > div.row > div.col-md-5 > div.cards.container > div:nth-child(1)");
+    const cards = document.querySelector("#content > div.row > div.col-md-5 > div.cards.container");
     let array = Array.from(elements)
     let y = array.sort(function (a, b) { return a.dataset.price - b.dataset.price });
     cards.innerHTML = ""
@@ -19,7 +19,7 @@ const desc = () => {
   let btn = document.getElementById("descending");
   btn.addEventListener("click", (event) => {
     const elements = document.querySelectorAll(".card-trip");
-    const cards = document.querySelector("#content > div.row > div.col-md-5 > div.cards.container > div:nth-child(1)");
+    const cards = document.querySelector("#content > div.row > div.col-md-5 > div.cards.container");
     let array = Array.from(elements)
     let y = array.sort(function (a, b) { return b.dataset.price - a.dataset.price });
     cards.innerHTML = ""
